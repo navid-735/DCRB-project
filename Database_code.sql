be40d4febb7480e0f17ab4327c3cedc6b254a4aa
Mohammad Navid Rizaei <165142627+navid-735@users.noreply.github.com>
2024-05-31 21:10:32 +0200

    The database schema

--- Database_code.sql
+++ Database_code.sql
@@ -1,11 +1,15 @@
+
+
+
 CREATE DATABASE IF NOT EXISTS localFile;
 
 USE localFile;
 
 CREATE TABLE IF NOT EXISTS files (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    file_path VARCHAR(255) NOT NULL UNIQUE,
-    file_name VARCHAR(255) NOT NULL,
-    file_type ENUM('directory', 'file') NOT NULL,
-    file_content TEXT
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    file_path VARCHAR(255) UNIQUE,
+    file_name VARCHAR(255),
+    file_type ENUM('directory', 'file', 'html', 'txt', 'py', 'jpg', 'png'),
+    file_size INT,
+    file_content LONGTEXT 
 );
